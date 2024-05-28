ab44cb1e0e8b7f40bcd27c81d0f47b10da7ed139
rinsha-aish <116577884+rinsha-aish@users.noreply.github.com>
2024-05-28 14:24:42 +0530

    initial_commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import Quiz from './Components/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <div>
+      <Quiz />
+      
+    </div>
+  )
+}
+
+export default App
